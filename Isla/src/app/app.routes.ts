a5972fe3be5f76fab2169744c16dce2e7426f11d
Jesus <8441@itescam.edu.mx>
2025-11-09 12:01:46 -0600

    DIosito funcionaX2

--- Isla/src/app/app.routes.ts
+++ Isla/src/app/app.routes.ts
@@ -23,14 +23,9 @@ import { EditHome } from './pages/admin/edit-home/edit-home';
 import { SeguimientoPedidosComponent } from './pages/client/seguimiento-pedidos/seguimiento-pedidos';
 import { AdminTrackingComponent } from './pages/admin/admin-tracking/admin-tracking';
 import { UpStockAmdPage } from './pages/admin/up-stock-amd/up-stock-amd';
-<<<<<<< HEAD
-=======
+
 import { Component } from '@angular/core';
 import { GestorUsuario } from './shared/gestor-usuario/gestor-usuario';
-
-
->>>>>>> 955cbce7960e166d21af940c863435e1f5219b42
-
 export const routes: Routes = [
   {
     path: '',
@@ -71,7 +66,6 @@ export const routes: Routes = [
     ]
   },
 
-<<<<<<< HEAD
   {
     path: 'home', 
     component: Home,
@@ -81,9 +75,7 @@ export const routes: Routes = [
       { path: 'food', component: Food },
     ]
   },
-=======
   { path: 'Drink', component: Drink},
->>>>>>> 955cbce7960e166d21af940c863435e1f5219b42
 
   // Direct route for cashier checkout
   { path: 'cashier-checkout', component: CashierCheckout },
